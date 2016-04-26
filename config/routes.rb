6ca1731b8dd08019a0b8c557661671af7a11f0a5
Jeff <jogan216@gmail.com>
2016-04-26 03:41:38 +0000

    incomplete forms - branch = login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
   get 'invitees/new'
 
   root 'static_pages#index'
@@ -9,6 +11,14 @@ Rails.application.routes.draw do
   get 'response' => 'response#rsvp'
   get 'garb' => 'static_pages#garb'
   get 'contact' => 'static_pages#contact'
+  
+  resources :users
+  
+  #login page routes
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
