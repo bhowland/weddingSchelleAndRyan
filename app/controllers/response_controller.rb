f80d6e62519e3f178d2aafb6b791551d976f9ca7
bhowland <benjhowland@gmail.com>
2016-04-28 16:49:30 -0500

    form working

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,6 +1,29 @@
 class ResponseController < ApplicationController
 
 def rsvp
+
+
 end
-  
+
+def new
+@invitee = Invitee.new
+create if request.post?
+end
+
+def create
+@invitee = Invitee.create(params[:invitee])
+redirect_to :action => 'edit', :id => @invitee.id
+end
+
+def edit
+@invitee = Invitee.find(params[:id])
+
+if request.post?
+  @invitee.update_attributes(params[:invitee])
+  redirect_to :action => 'edit', :id => @invitee.id and return
+end
+
+  render :action => 'new'
+end
+
 end
