41e9e99a06673d95d0d43c60740cc2b017bb1938
Jeff <jogan216@gmail.com>
2016-05-02 20:41:13 +0000

    delete fixed - goto master

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,57 +11,12 @@ class AdminController < ApplicationController
   def show
     @invitee = Invitee.find(params[:id])
   end
-  
-  #def delete
-   # Invitee.find_by(id: params[:id]).destroy
-    #redirect_to admin
-  #end 
-  
+
   def destroy
     @id = Invitee.find_by(params[:id])
     @invitee = Invitee.find_by(params[@id])
     @invitee.destroy
     redirect_to request.referrer
   end
-  
-
-  # def create
-  #   #render new
-  #   #@invitee = Invitee.all
-  #   #redirect_to @invitee
-  # end
-  
-  # def index
-  # end
-  
-  
-  
-  
-# begin   
-#   def new
-#     @invitee = Invitee.new
-#     create if request.post?
-#   end
-  
-#   def create
-#     @invitee = Invitee.create(params[:invitee])
-#     redirect_to :action => 'edit', :id => @invitee.id
-#   end
-  
-#   def edit
-#     @invitee = Invitee.find(params[:id])
-    
-#     if request.post?
-#       @invitee.update_attributes(params[:invitee])
-#       redirect_to :action => 'edit', :id => @invitee.id and return
-#     end
-    
-#       render :action => 'new'
-#   end
-  
-#   def delete
-    
-#   end
-# end    
-  
+ 
 end
