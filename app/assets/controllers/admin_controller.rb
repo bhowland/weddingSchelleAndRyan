51d73dc7ef7b5185eda310aeb64f8add91e836bc
bhowland <benjhowland@gmail.com>
2016-05-02 19:57:04 -0500

    clean some code

--- app/assets/controllers/admin_controller.rb
+++ app/assets/controllers/admin_controller.rb
@@ -1,9 +1,5 @@
 class AdminController < ApplicationController
-  
-  # def index
-  #   @invitee = Invitee.all
-  # end
-  
+      
   def admin
     @invitee = Invitee.all
   end
@@ -11,28 +7,4 @@ class AdminController < ApplicationController
   def show
     @invitee = Invitee.find(params[:id])
   end
-
-
-  def destroy
-
-    @invitee = Invitee.find_by(params[:id])
-    @invitee.destroy
-    redirect_to admin_path
-  end
-####################################################################
-  
-  def new
-    @invitee = Invitee.new
-  end
-
-  
-  def show
-    @invitee = Invitee.find(params[:id])
-  end
-  
-  def edit
-    @invitee = Invitee.find(params[:id])
-  end
-
-
 end
--- app/assets/controllers/invitee_controller.rb
+++ app/assets/controllers/invitee_controller.rb
@@ -1,20 +1,16 @@
 class InviteeController < ApplicationController
   
-  def index
-    
-  end
-  
-  def admin
-    
-  end
-  
-  def show
-   
-  end
-  
   def destroy
     Invitee.find(params[:id]).destroy
     redirect_to request.referrer
   end
   
+  def create
+    @invitee = Invitee.create(db)
+    redirect_to request.referrer 
+  end
+
+  def db
+    params.permit(:name_on_invite, :invitees_present, :plus_ones, :dressed_in_garb, :phone, :email)
+  end
  end
