6eff5e22021f31ccc5f9c56c5d7e358610d3a5fa
bhowland <benjhowland@gmail.com>
2016-05-02 19:35:39 -0500

    clean some code

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,19 +1,3 @@
 class ResponseController < ApplicationController
 
-  def show
-    @invitee = Invitee.find(params[:id])
-  end
-  
-  def new
-    @invitee = Invitee.new
-    create if request.post?
-  end
-
-  def create
-    
-    @invitee = Invitee.create(params[:invitee])
-    redirect_to request.referrer
-    
-  end
-
 end
